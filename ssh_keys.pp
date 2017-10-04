90cff89c5ea38119220e104e0294b052f84b5a2e
Loellis <loellis92@gmail.com>
2017-10-04 21:04:07 +0200

    Delete ssh_keys.pp

--- ssh_keys.pp
+++
@@ -1,5 +0,0 @@
-ssh_authorized_key { "master_root" :
-        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCl5/bHNCMRClCOYJIIp++HwyRf9O0ChwDx8e2OdOcBu5HmxHaDMAx4wOA/9s77ShX3g0VWBjzl+2L6ymp86gSpTX/AXp6FKlfQ6bEjspCvQlN2gGixB/EdRkg$        user => root,
-        ensure => present,
-        type => rsa,
-}
