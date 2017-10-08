161cf6d9eec91baec9ae39b8e00f1a6106c6a77c
Loellis <loellis92@gmail.com>
2017-10-08 16:22:36 +0200

    Update ssh_keys.pp

--- ssh_keys.pp
+++ ssh_keys.pp
@@ -1,12 +1,12 @@
 ssh_authorized_key { "master_root" :
-        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCl5/bHNCMRClCOYJIIp++HwyRf9O0ChwDx8e2OdOcBu5HmxHaDMAx4wOA/9s77ShX3g0VWBjzl+2L6ymp86gSpTX/AXp6FKlfQ6bEjspCvQlN2gGixB/EdRkgH7DYEjO8hPuUd1g6XZZ4FoJG/2vFa5oCOFiEn/xu0LsC3gJuhlGBGfQRnmfYWB++ESqKTOkcjrAB/MtxwibdPSjhyqsWoLgO3MKAiUc5/HJrDBrNPUrMG+aogvqn9gE1RCAQ04lcxjWorNePIpcUAYoWPJo1sMqISFzBD4MBuWvr4nOX6sIEpjaCMQf4rM+cRuFCzXq97cxJLXuhMlzmm6uUB04Ov",        
+        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCaN2/XCWx1orO7gf+QgIsvHM8zFelTsKdWYb19fBqilal94EUX3RPuMEkoAEa6tUTVWX+YA24hUAWvSd3/ZBVwWcOy9rAQOvQddaQ3mAyKQEJaJQPksguWsr/eGoRfn54XlMAjoXbW0wTyqJF9Kjq7eTk+4misYzQVjub8B9lrzgFs5v7xgVRmMwyLcF4xUzdfnNSPO9wkKZnPPZVSoeCXu6riw6SsYNoQwcRsp/3pt+sKuU6TOewbP6pgfiZ8fcktd2m29hfWuwjNwiVybZWIvGlyyvBWlLjivoWuulMhu6D5caxSiyX/xPBAQ4JswvOIorsAHcuZ1C28vGeYmM1r",        
 	user => root,
         ensure => present,
         type => rsa,
 }
 
 ssh_authorized_key { "master_ubuntu" :
-        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCl5/bHNCMRClCOYJIIp++HwyRf9O0ChwDx8e2OdOcBu5HmxHaDMAx4wOA/9s77ShX3g0VWBjzl+2L6ymp86gSpTX/AXp6FKlfQ6bEjspCvQlN2gGixB/EdRkgH7DYEjO8hPuUd1g6XZZ4FoJG/2vFa5oCOFiEn/xu0LsC3gJuhlGBGfQRnmfYWB++ESqKTOkcjrAB/MtxwibdPSjhyqsWoLgO3MKAiUc5/HJrDBrNPUrMG+aogvqn9gE1RCAQ04lcxjWorNePIpcUAYoWPJo1sMqISFzBD4MBuWvr4nOX6sIEpjaCMQf4rM+cRuFCzXq97cxJLXuhMlzmm6uUB04Ov",        
+        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCaN2/XCWx1orO7gf+QgIsvHM8zFelTsKdWYb19fBqilal94EUX3RPuMEkoAEa6tUTVWX+YA24hUAWvSd3/ZQddaQ3mAyKQEJaJQPksguWsr/eGoRfn54XlMAjoXbW0wTyqJF9Kjq7eTk+4misYzQVjub8B9lrzgFs5v7xgVRmMwyLcF4xUzdfnNSPO9wkKZnPPZVSoeCXu6riw6SsYNoQwcRsp/3pt+sKuU6TOewbP6pgfiZ8fcktd2m29hBVwWcOy9rAQOvfWuwjNwiVybZWIvGlyyvBWlLjivoWuulMhu6D5caxSiyX/xPBAQ4JswvOIorsAHcuZ1C28vGeYmM1r",        
 	user => ubuntu,
         ensure => present,
         type => rsa,
