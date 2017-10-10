7990172204061b30606b06bcc2b4fa45da2ec23e
Loellis <loellis92@gmail.com>
2017-10-10 08:55:33 +0200

    Update puppet_agent.pp

--- puppet_agent.pp
+++ puppet_agent.pp
@@ -1,6 +1,6 @@
 $master = "master.openstacklocal"
 $master_ip = "10.0.8.71"
-$interval = "240"
+$interval = "300"
 
 host { 'master.openstacklocal' :
   name => $master,
@@ -19,7 +19,7 @@ augeas { "puppet.conf" :
   context =>  "/files/etc/puppet/puppet.conf",
   changes => [
     "set agent/server $master",
-    "set agent/runinterval 240",
+    "set agent/runinterval 300",
   ],
   notify => Service['puppet'],
 }
