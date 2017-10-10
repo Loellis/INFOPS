c67f8e5e75f4817d86c7d67e67a60889ff04d9f6
Loellis <loellis92@gmail.com>
2017-10-10 10:32:26 +0200

    Update ssh_root_key.pp

--- ssh_root_key.pp
+++ ssh_root_key.pp
@@ -1,6 +1,6 @@
 ssh_authorized_key { "master_root" :
-        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC5lbMK8ejIC0P6NZPE/GbCZo2VWTwz030QVn1esDWQOi3fvsd7vAikXhxqwZiCSKInHUd4qREboh3l6RyLSlpygj5KEZlbhJh9CLrd5E61x3OMU5Ms7L4ZoF9879hpqF8d7GGM54XKqkMqMAMK0VeafidhiCiGgRjVhMIz6sZZ8YJgcEQkmefWm1n4sitKYaJIRkFRbUb9rEjs9+G9Wq/FGaDP7HKu8AslOzEZXgn9JKYCoClvk7AvNj7Sri2vgR9RW0ndteekhHgVjb9FZMAl4Tok9Ph3I0XmTrm/83IFQJ+4lHyCmq0AuoYOK8Txxn65i1KkWmRxQP7prR+67Nnp",        
-	      user => root,
+  	key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDBsVIrwySiL5FK1kSRnQ3x0pojccoNBZ5i/lVpFq0Cf9xxzThuuwFBCsoIpQFTHQ5eN7iurv7MGuzwyyKCLP79yg6pWyZNVHY7AioTqiWMRwwzSUKKLEiRYyDZK/J7nr1Q+KjEBIOKC8iG/aVN2uJZn/mLVPdc4PE6qdBGflP9Alyxl6WoEMZcttMzyne3dK1JuJAtJE2ESRRwu6CnZbmYmsp/hWevV+3TkwUUKkwWM+phM1Q6T0qh27XQNdnEbOKZwdY2GFBM2ExHMmpvtU1RaIZXW6z2F+rxMNR5ByPjWKYsHRAs9E7wUPrBFbqRmRg5CBKGwjtIqmQ7Se1jg8rh",
+        user => ubuntu,
         ensure => present,
         type => rsa,
 }
