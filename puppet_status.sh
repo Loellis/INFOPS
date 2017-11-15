841c3ee1e701378f1e1f915dde88b623b4fca67f
Loellis <loellis92@gmail.com>
2017-11-15 06:06:06 +0100

    Update puppet_status.sh

--- puppet_status.sh
+++ puppet_status.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-status=$(sudo cat /var/lib/puppet/state/last_run_summary.yaml | grep failure | awk '{print $2}')
+for STAT in $(sudo cat /var/lib/puppet/state/last_run_summary.yaml | grep fail | awk '{print $2}')
+do
+        if [ "$STAT" -gt 0 ]
+        then
+                echo "Last puppet run did not finish without errors."
+                exit 2
+        fi
+done
 
-if [ "$status" -gt 0 ]
-then
-        echo "Last puppet run did not finish without errors."
-        exit 2
-else
-        echo "Last puppet run successful."
-        exit 0
-fi
+echo "Last puppet run successful."
+exit 0
+
 
